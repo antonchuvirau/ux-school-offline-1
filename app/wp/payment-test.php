0ecf017961947604bf81a8cdc68b40126638186e
antonchuvirau <anton_chuvirau@icloud.com>
2021-04-10 20:23:28 +0300

    Added changes to ERIP payment

--- app/wp/payment-test.php
+++ app/wp/payment-test.php
@@ -80,6 +80,32 @@ $is_promocode = get_field('promocode_bool', 2);
 									<section class="payment-section payment-form__section">
 										<div class="erip-payment payment-form__section-item">
 											<span class="payment-form__section-name">3. Как оплатить через ЕРИП</span>
+											<div class="erip-payment__wrapper">
+												<div class="erip-payment__price">Сумма для оплаты<span class="erip-payment__price-value">790 BYN</span></div>
+												<div class="erip-payment__grid">
+
+												</div>
+												<p class="payment-message erip-payment__message">*Скидки не суммируются.<br/>После внесения платежа, отправьте копию квитанции на <a href="mailto:hello@ux-school.by">hello@ux-school.by</a></p>
+												<div class="erip-payment__content">
+													<div class="erip-payment__content-wrapper">
+														<p>Как найти нас в ЕРИП:</p>
+														<ul>
+															<li>1. Пункт "Система "Расчет" (ЕРИП)</li>
+															<li>2. Образование и развитие</li>
+															<li>3. Дополнительное образование и развитие</li>
+															<li>4. Тренинги, семинары, консультации</li>
+															<li>5. Минск</li>
+															<li>6. ИП Колесень И.Г.</li>
+															<li>7. Посещение занятий</li>
+															<li>8. Ввести ФИО ученика и сумму для оплаты</li>
+														</ul>
+													</div>
+													<figure class="erip-payment__qr-code">
+														<img class="erip-payment__qr-code-img" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/qr-code.svg" alt="UX Mind School - оплата курсов с помощью ЕРИП">
+														<figcaption class="erip-payment__qr-code-note">Код услуги: 4725501</figcaption>
+													</figure>
+												</div>
+											</div>
 										</div>
 									</section>
 									<!-- END ERIP -->
