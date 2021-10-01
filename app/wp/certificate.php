4a00de0440aeb280c54acd6d04b048f4f04581b1
Surkov Pavel <surkovv.pavel@gmail.com>
2021-10-01 21:02:01 +0700

    fixes issues from Igor's video

--- app/wp/certificate.php
+++ app/wp/certificate.php
@@ -12,7 +12,7 @@
 		<div class="row no-gutters align-items-center">
 			<div class="order-2 order-lg-1 col-12 col-lg-5">
 				<img src="<?php echo get_template_directory_uri(); ?>/img/ums-baloon-img.svg" class="d-none d-lg-block certificate__baloon-img" alt="<?php echo get_bloginfo('name'); ?>">
-				<p class="certificate__text">По окончании курса студенты, защитившие итоговую работу, получают именной сертификат от UX Mind School</p>
+				<p class="certificate__text">По окончании курса студенты, защитившие итоговую работу, получают сертификат от UX Mind School</p>
 			</div>
 			<div class="order-1 order-lg-2 col-12 col-lg-7">
 				<div class="certificate__img">
