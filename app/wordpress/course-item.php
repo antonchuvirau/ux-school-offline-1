0b660e640b087ea8adbc77b0f908e4f5a8cf7d7b
Pavel-Surkov <surkovv.pavel@gmail.com>
2021-11-17 16:02:16 +0700

    fixes course-list at the main page

--- app/wordpress/course-item.php
+++ app/wordpress/course-item.php
@@ -97,7 +97,7 @@ $course_start_date_timestamp = $course_start_date_object->getTimestamp();
 				</p>
 				<?php if ( $course_sale_price && !$is_course_free ): ?>
 				<div class="course-list-item__meta-item">
-					<div data-title="Цена:" data-title-lg="Стоимость" class="course-list-item__meta-value course-list-item__meta-price" data-price="<?php echo $course_sale_price; ?>">
+					<div data-title="Цена:" data-title-lg="Цена" class="course-list-item__meta-value course-list-item__meta-price" data-price="<?php echo $course_sale_price; ?>">
 						<div>
 							<span class="course-list-item__meta-inner"><?php echo $course_price; ?></span>
 							<span><?php echo $course_sale_price; ?><?php echo CURRENCY_CODE; ?></span>
@@ -115,7 +115,7 @@ $course_start_date_timestamp = $course_start_date_object->getTimestamp();
 				</div>
 				<?php elseif ( !$course_sale_price && !$is_course_free ): ?>
 					<div class="course-list-item__meta-item">
-						<p data-title="Цена:" data-price="<?php echo $course_price; ?>" class="course-list-item__meta-value course-list-item__meta-price"><?php echo $course_price; ?> <?php echo CURRENCY_CODE; ?></p>
+						<p data-title="Цена:" data-title-lg="Цена" data-price="<?php echo $course_price; ?>" class="course-list-item__meta-value course-list-item__meta-price"><?php echo $course_price; ?> <?php echo CURRENCY_CODE; ?></p>
 						<div class="info course-list-item__meta-info">
 							<svg class="info__icon" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
 								<path d="M8 0C3.5888 0 0 3.5888 0 8C0 12.4112 3.5888 16 8 16C12.4112 16 16 12.4112 16 8C16 3.5888 12.4112 0 8 0ZM9 12H7V7H9V12ZM9 6H7V4H9V6Z" fill="#E0E6EB"/>
@@ -127,9 +127,10 @@ $course_start_date_timestamp = $course_start_date_object->getTimestamp();
 						</div>
 					<div>
 				<?php else: ?>
-					<p class="course-list-item__meta-item">
-						<span data-title="Цена:" class="course-list-item__meta-value course-list-item__meta-price"><?php echo $course_price; ?></span>
-					</p>
+					<div class="course-list-item__meta-item">
+						<span data-title="Цена:" data-title-lg="Цена" class="course-list-item__meta-value course-list-item__meta-price">Бесплатно</span>
+						<!-- <?php echo $course_price; ?> -->
+					</div>
 				<?php endif; ?>
 			</div>
 		</div>
