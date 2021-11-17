7dc0546d677cc9aa39f74c56c971034352738f5e
Pavel-Surkov <surkovv.pavel@gmail.com>
2021-11-17 16:35:12 +0700

    adds changes from notion

--- app/wordpress/advantages.php
+++ app/wordpress/advantages.php
@@ -12,25 +12,33 @@
 		<div class="row">
 			<div class="col-6 col-lg-3 advantages__col">
 				<div class="advantages__item">
-					<img class="advantages__img" src="<?php echo get_template_directory_uri(); ?>/img/icons/feature-1.svg" alt="<?php echo esc_attr( get_bloginfo('name') ); ?>">
+					<div class="advantages__img">
+						<img src="<?php echo get_template_directory_uri(); ?>/img/icons/money.svg" alt="<?php echo esc_attr( get_bloginfo('name') ); ?>">
+					</div>
 					<p class="advantages__name">Скидка 10% студентам&nbsp;-&nbsp;очникам</p>
 				</div>
 			</div>
 			<div class="col-6 col-lg-3 advantages__col">
 				<div class="advantages__item">
-					<img class="advantages__img" src="<?php echo get_template_directory_uri(); ?>/img/icons/feature-2.svg" alt="<?php echo esc_attr( get_bloginfo('name') ); ?>">
+					<div class="advantages__img">
+						<img src="<?php echo get_template_directory_uri(); ?>/img/icons/cup.svg" alt="<?php echo esc_attr( get_bloginfo('name') ); ?>">
+					</div>
 					<p class="advantages__name">Рассрочка</br>до&nbsp;6&nbsp;месяцев</p>
 				</div>
 			</div>
 			<div class="col-6 col-lg-3 advantages__col">
 				<div class="advantages__item">
-					<img class="advantages__img" src="<?php echo get_template_directory_uri(); ?>/img/icons/feature-3.svg" alt="<?php echo esc_attr( get_bloginfo('name') ); ?>">
+					<div class="advantages__img">
+						<img src="<?php echo get_template_directory_uri(); ?>/img/icons/student.svg" alt="<?php echo esc_attr( get_bloginfo('name') ); ?>">
+					</div>
 					<p class="advantages__name">Занятие 2-3 часа с&nbsp;перерывом на кофе</p>
 				</div>
 			</div>
 			<div class="col-6 col-lg-3 advantages__col">
 				<div class="advantages__item">
-					<img class="advantages__img" src="<?php echo get_template_directory_uri(); ?>/img/icons/feature-4.svg" alt="<?php echo esc_attr( get_bloginfo('name') ); ?>">
+					<div class="advantages__img">
+						<img src="<?php echo get_template_directory_uri(); ?>/img/icons/rock.svg" alt="<?php echo esc_attr( get_bloginfo('name') ); ?>">
+					</div>
 					<p class="advantages__name">Скидка 10% ученикам&nbsp;школы</p>
 				</div>
 			</div>
