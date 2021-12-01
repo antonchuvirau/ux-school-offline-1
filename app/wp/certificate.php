d80c8f5078d32be7f068dae3374053bd5d2af3dd
Pavel Surkov <surkovv.pavel@gmail.com>
2021-12-01 20:43:36 +0700

    adds new certificate block

--- app/wp/certificate.php
+++ app/wp/certificate.php
@@ -9,10 +9,12 @@
 ?>
 <div class="certificate <?php if ( is_page_template('home.php') ): ?>home<?php else: ?>course<?php endif; ?>-template__certificate">
 	<div class="container">
-		<div class="row no-gutters align-items-center">
-			<div class="order-2 order-lg-1 col-12 col-lg-5">
-				<img src="<?php echo get_template_directory_uri(); ?>/img/ums-baloon-img.svg" class="d-none d-lg-block certificate__baloon-img" alt="<?php echo get_bloginfo('name'); ?>">
-				<p class="certificate__text">По окончании курса студенты, защитившие итоговую работу, получают сертификат от UX Mind School</p>
+		<div class="row no-gutters justify-content-end">
+			<div class="order-2 order-lg-1 col-12 col-lg-4">
+				<!-- <img src="<?php echo get_template_directory_uri(); ?>/img/ums-baloon-img.svg" class="d-none d-lg-block certificate__baloon-img" alt="<?php echo get_bloginfo('name'); ?>"> -->
+				<p class="d-none d-lg-block certificate__title">Сертификат</p>
+				<p class="certificate__text">По окончании курса, студенты защитившие итоговую работу получают именной сертификат от UX Mind School</p>
+				<p class="certificate__note">А лучшим выпускникам предлагаем оплачиваемую стажировку и работу в нашей студии дизайна UX&nbsp;Mind&nbsp;и&nbsp;компаний, с которыми мы сотрудничаем</p>
 			</div>
 			<div class="order-1 order-lg-2 col-12 col-lg-7">
 				<div class="certificate__img">
